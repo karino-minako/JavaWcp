90ee3aa31f9fc3dda832fac3cc938478c3f9056f
karino-minako <irikiiomo11@gmail.com>
2020-09-06 15:32:38 +0900

    [Finish] kiso-4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,24 @@
+//public class Chapter04 {
+//  public static void main(String[] args) {
+//	String str1;
+//	str1 = "Karino Minako";
+//	System.out.println(str1);
+//	str1 = "Finish";
+//	System.out.println(str1);
+//	
+//	int num1 = 30;
+//	System.out.println(num1);
+//  }
+//}
+
+public class Chapter04 {
+  public static void main(String[] args) {
+	final int LUCKY_NUMBER = 7;
+	String str = "Karino Minako";
+	System.out.println(str);
+
+	str = "Finish";
+	System.out.println(str);
+	System.out.println(LUCKY_NUMBER);
+  }
+}
